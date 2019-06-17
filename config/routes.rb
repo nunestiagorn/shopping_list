42625ddf17258a30e78acf15b09d7c1769af2a2b
Tiago <nunestiagorn@gmail.com>
2019-06-16 21:19:57 -0300

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :lists
+  resources :lists do 
+    resources :items
+  end
+  root 'lists#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
